01c3e18a9f5912a37d09fdf0cbfc32baac9462b4
Kevin Wang <kwangsan@gmail.com>
2021-09-12 11:56:56 -0400

    chore: nest utils in src

---
+++ src/utils/mdxUtils.js
@@ -0,0 +1,11 @@
+import fs from "fs";
+import path from "path";
+
+// DOCS_PATH is useful when you want to get the path to a specific file
+export const DOCS_PATH = path.join(process.cwd(), "docs/source");
+
+// postFilePaths is the list of all mdx files inside the DOCS_PATH directory
+export const docsFilePaths = fs
+  .readdirSync(DOCS_PATH)
+  // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path));
