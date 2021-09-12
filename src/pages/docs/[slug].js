bb81cf77e97162fdf4cf920d5e228c22ff69ce6f
Kevin Wang <kwangsan@gmail.com>
2021-09-12 11:57:19 -0400

    chore: rename posts to docs

--- src/pages/docs/[slug].js
+++ src/pages/docs/[slug].js
@@ -1,14 +1,13 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import { MDXRemote } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import Link from 'next/link'
-import path from 'path'
-import CustomLink from '../../components/CustomLink'
-import Layout from '../../components/Layout'
-import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils'
+import fs from "fs";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Head from "next/head";
+import Link from "next/link";
+import path from "path";
+import CustomLink from "../../components/CustomLink";
+import Layout from "../../components/Layout";
+import { docsFilePaths, DOCS_PATH } from "../../utils/mdxUtils";
 
 // Custom components/renderers to pass to MDX.
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
@@ -16,12 +15,8 @@ import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils'
 // here.
 const components = {
   a: CustomLink,
-  // It also works with dynamically-imported components, which is especially
-  // useful for conditionally loading components for certain routes.
-  // See the notes in README.md for more details.
-  TestComponent: dynamic(() => import('../../components/TestComponent')),
   Head,
-}
+};
 
 export default function PostPage({ source, frontMatter }) {
   return (
@@ -56,14 +51,14 @@ export default function PostPage({ source, frontMatter }) {
         }
       `}</style>
     </Layout>
-  )
+  );
 }
 
 export const getStaticProps = async ({ params }) => {
-  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`)
-  const source = fs.readFileSync(postFilePath)
+  const postFilePath = path.join(DOCS_PATH, `${params.slug}.md`);
+  const source = fs.readFileSync(postFilePath);
 
-  const { content, data } = matter(source)
+  const { content, data } = matter(source);
 
   const mdxSource = await serialize(content, {
     // Optionally pass remark/rehype plugins
@@ -72,25 +67,25 @@ export const getStaticProps = async ({ params }) => {
       rehypePlugins: [],
     },
     scope: data,
-  })
+  });
 
   return {
     props: {
       source: mdxSource,
       frontMatter: data,
     },
-  }
-}
+  };
+};
 
 export const getStaticPaths = async () => {
-  const paths = postFilePaths
+  const paths = docsFilePaths
     // Remove file extensions for page paths
-    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((path) => path.replace(/\.mdx?$/, ""))
     // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }))
+    .map((slug) => ({ params: { slug } }));
 
   return {
     paths,
     fallback: false,
-  }
-}
+  };
+};
